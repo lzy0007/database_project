24723dfe704238c9dc49fb0f2f7b9fcc15bb6263
NG ZI XING <92634733+ngzxing@users.noreply.github.com>
2024-01-08 15:16:44 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -11,7 +11,7 @@ from utils import getOne_query, getMany_query
 
 st.write("# Sakila Dashboard")
 group = st.expander("Group description")
-group.table(pd.DataFrame({"Name": ["NG ZI XING", "LOO ZHI YUAN", "SAM CHIA YUN", "YEW RUI XIANG"], "Matric Code": ["A21EC0213", "A21EC0197", "A21EC", "A21EC0149"] }))
+group.table(pd.DataFrame({"Name": ["NG ZI XING", "LOO ZHI YUAN", "SAM CHIA YUN", "YEW RUI XIANG"], "Matric Code": ["A21EC0213", "A21EC0197", "A21EC0127", "A21EC0149"] }))
 
 overview_tab, film_tab, customer_tab, staff_tab, store_tab = st.tabs(["##### Overview", "##### Film", "##### Customers", "##### Staffs", "##### Store"])
 
