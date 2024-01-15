9841415797426e629d24e19571e0164ade686eb0
YEW RUI XIANG <146644356+yruixiang@users.noreply.github.com>
2024-01-15 12:00:32 +0800

    Update store.py

--- store.py
+++ store.py
@@ -6,4 +6,28 @@ from utils import getOne_query, getMany_query
 
 def store():
 
-    pass
+    def row_metrics(title, queries):
+
+        columns = st.columns(len(title))
+
+        for t, c, q in zip(title, columns, queries):
+            c.metric(t, str( getOne_query(q) ).capitalize() )
+        
+
+    row_metrics(["Total Number of Store", "Highest Sales Store ID", "Highest Rental Store ID"],
+                    [ getattr(queries, f"store_{m}") for m in ["totalStore", "highSalesStore", "highRentalStore"] ])
+    
+    
+    st.write("## List of Staff in Each Store")
+    st.dataframe(getMany_query( getattr(queries, "store_listStaffStore") ), use_container_width = True)
+
+
+    st.write("## Details for each store")
+    details = {"Number of staff" : "numStaff", "Number of inventory" : "numInventory", 
+    "Number of rental" : "numRental", "Number of sales" : "numSales", "Number of customer" : "numCustomer"}
+    column_y = {"Number of staff" : "no_of_staff", "Number of inventory" : "no_of_inventory", 
+    "Number of rental" : "no_of_rental", "Number of sales" : "no_of_sales", "Number of customer" : "no_of_customer"}
+    key = st.selectbox("Select details", details)
+
+    st.bar_chart( getMany_query( getattr(queries, f"store_{details[key]}EachStore") ), y = column_y[key], x = "store_id" )
+
